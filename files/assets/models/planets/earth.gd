bd585ed656aea6eb6f8181c403d29295e504b77a
PonderSlime <micah.j.edwards1@gmail.com>
2024-08-10 19:59:59 -0600

    Planet earth added, NASA textures modified

---
+++ files/assets/models/planets/earth.gd
@@ -0,0 +1,11 @@
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+	$Planet.rotation_degrees.y -= 0.0001851851851851851852
