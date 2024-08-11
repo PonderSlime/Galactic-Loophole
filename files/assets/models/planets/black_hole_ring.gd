b4dd82e091469d996a8911a3a1374228b4d44be0
PonderSlime <micah.j.edwards1@gmail.com>
2024-08-11 11:59:14 -0600

    Black hole added, engine flame shader added

---
+++ files/assets/models/planets/black_hole_ring.gd
@@ -0,0 +1,11 @@
+extends Node3D
+
+@export var player : CharacterBody3D
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	look_at(player.global_transform.origin, Vector3.UP)
