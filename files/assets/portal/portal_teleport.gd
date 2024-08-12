12057d884643a8082e53c192e5ea3474820fddac
PonderSlime <micah.j.edwards1@gmail.com>
2024-08-11 20:49:11 -0600

    More space station segments added

--- files/assets/portal/portal_teleport.gd
+++ files/assets/portal/portal_teleport.gd
@@ -14,10 +14,3 @@ func _on_area_entered(area:Area3D):
 	if area.has_meta("teleportable_root"):
 		var root:Node3D = area.get_node(area.get_meta("teleportable_root"))
 		root.global_transform = _parent_portal.real_to_exit_transform(root.global_transform)
-		var cam:Camera3D = root.get_node("Camera")
-		cam.rotation.x = root.rotation.x
-		
-		root.rotation.z = 0
-		await get_tree().create_timer(0.001).timeout
-		root.rotation.x = 0
-		print("zoom!")
