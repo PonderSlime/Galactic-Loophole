c5c4520f2f72ad1345f5c240ba7498b34b93ef32
PonderSlime <micah.j.edwards1@gmail.com>
2024-08-12 21:06:46 -0600

    Light flicker added, protal transition improved

--- files/assets/station/segments/light_flicker.gd
+++ files/assets/station/segments/light_flicker.gd
@@ -1,13 +1,20 @@
-extends CSGBox3D
-
-var light_flicker_ammount = 0
+extends Node3D
+var light_flicker_amount = 0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
+	pass
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	light_flicker_ammount = randi_range(0,100)
-	#if light_flicker_ammount < 50:
-		
+	light_flicker_amount = randf_range(0,100)
+	await get_tree().create_timer(10).timeout
+	if light_flicker_amount <= 5:
+		$LightStripLit.set_instance_shader_parameter("shader_parameter/brightness",0)
+		$SpotLight3D.light_energy = 0.1
+		$LightStripLit.visible = false
+		await get_tree().create_timer(100).timeout
+	else:
+		$LightStripLit.set_instance_shader_parameter("shader_parameter/brightness",2)
+		$SpotLight3D.light_energy = 2.5
+		$LightStripLit.visible = true
+		await get_tree().create_timer(150).timeout
+	
