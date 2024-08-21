21d649be489f1d33edb02858f948eb57dfb6cc3d
PonderSlime <micah.j.edwards1@gmail.com>
2024-08-20 19:50:22 -0600

    Menu issues fixed & ready for more additions,, new font added

--- files/screens/pause_screen.gd
+++ files/screens/pause_screen.gd
@@ -6,27 +6,32 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 var pause_game : bool = false
+var options: bool = false
 # Called every frame. 'delta' is the elapsed time since the previous frame.
+@onready var exit_but = $OptionsScreen/MarginContainer/VBoxContainer/Button
 func _process(delta: float) -> void:
 	_is_paused()
 	if get_tree().paused:
-		visible = true
+		$ScreenVisibility.visible = true
 	else:
-		visible = false
+		$ScreenVisibility.visible = false
+		$OptionsScreen.visible = false
 		
 func _is_paused():
-	if Input.is_action_just_pressed("pause"):
+	if Input.is_action_just_pressed("pause") and options == false:
 		pause_game = !pause_game
 		get_tree().paused = !get_tree().paused
 	if pause_game == false:
 		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	elif pause_game == true:
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-	
 
 
 func _on_options_pressed() -> void:
 	print("options")
+	$OptionsScreen.visible = true
+	$ScreenVisibility.visible = false
+	options = true
 
 
 func _on_resume_pressed() -> void:
@@ -36,3 +41,9 @@ func _on_resume_pressed() -> void:
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_exit_pressed() -> void:
+	$OptionsScreen.visible = false
+	$ScreenVisibility.visible = true
+	options = false
